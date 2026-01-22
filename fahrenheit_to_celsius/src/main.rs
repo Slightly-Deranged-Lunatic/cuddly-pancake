19232281e8d39dcf1dda89a79a787c989c2665b9
Slightly-Deranged-Lunatic <110319723+Slightly-Deranged-Lunatic@users.noreply.github.com>
2026-01-21 22:45:50 -0600

    Finish fahrenheit to celsius

--- fahrenheit_to_celsius/src/main.rs
+++ fahrenheit_to_celsius/src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut fahrenheit_temperature = String::new();
+
+    let fahrenheit_temperature = loop {
+        println!("Please input the fahrenheit temperature: ");
+        io::stdin()
+            .read_line(&mut fahrenheit_temperature)
+            .expect("Failed to read line");
+
+        let fahrenheit_temperature: f64 = match fahrenheit_temperature.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
+        break fahrenheit_temperature;
+    };
+    let celsius_temperature = (fahrenheit_temperature - 32.0) /1.8;
+    println!("{fahrenheit_temperature} fahrenheit to celsius is {celsius_temperature}.");
 }
+
