0474338872f19703ae6bd46a44ea8c36ddbb26f7
Slightly-Deranged-Lunatic <110319723+Slightly-Deranged-Lunatic@users.noreply.github.com>
2026-01-27 03:57:26 -0600

    Rock paper scissors stuff too lazy to make a determine winners function

--- rock_paper_scissors/src/main.rs
+++ rock_paper_scissors/src/main.rs
@@ -1,3 +1,28 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to rock paper scissors");
+    println!("Player 1 please enter your move");
+    static VALID_INPUT : [&str; 3] = ["rock", "paper", "scissors"];
+    let player_one_action = get_player_action(VALID_INPUT);
+    clearscreen::clear().expect("failed to clear screen");
+    println!("Player 2 please enter your move");
+    let player_two_action = get_player_action(VALID_INPUT);
+    clearscreen::clear().expect("failed to clear screen");
+    }
+
+fn get_player_action(VALID_INPUT: [&str; 3]) -> String {
+    loop {
+        let mut player_action = String::new();
+        io::stdin()
+            .read_line(&mut player_action)
+            .expect("Failed to read line!");
+        player_action = player_action.trim().to_lowercase();
+        if ! VALID_INPUT.contains(&&player_action.as_str()) {
+            println!("Invalid input");
+        }
+        else {
+            return player_action
+        }
+    }
 }
